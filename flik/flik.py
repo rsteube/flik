b82cc2007796ed941f9a790eff16ec58bbcebdf2
rsteube <rsteube@users.noreply.github.com>
2016-12-19 23:50:34 +0100

    fixed completion error on incomplete date string

--- flik/flik.py
+++ flik/flik.py
@@ -162,24 +162,27 @@ def api():
     print masterDataService()
 
 def convertDate(raw_date):
-    weekday={
-            'monday': MO(-1),        
-            'tuesday': TU(-1),        
-            'wednesday': WE(-1),        
-            'thursday': TH(-1),        
-            'friday': FR(-1),        
-            'saturday': SA(-1),        
-            'sunday': SU(-1),        
-        }
-    if raw_date == 'today':
-        date = datetime.now().date()
-    elif raw_date == 'yesterday':
-        date = datetime.now().date() - relativedelta(days=1)
-    elif raw_date in weekday:
-        date = datetime.now().date() + relativedelta(weekday=weekday[raw_date])
-    else:
-        date = datetime.strptime(raw_date, '%Y-%m-%d')
-    return date
+    try:
+        weekday={
+                'monday': MO(-1),        
+                'tuesday': TU(-1),        
+                'wednesday': WE(-1),        
+                'thursday': TH(-1),        
+                'friday': FR(-1),        
+                'saturday': SA(-1),        
+                'sunday': SU(-1),        
+            }
+        if raw_date == 'today':
+            date = datetime.now().date()
+        elif raw_date == 'yesterday':
+            date = datetime.now().date() - relativedelta(days=1)
+        elif raw_date in weekday:
+            date = datetime.now().date() + relativedelta(weekday=weekday[raw_date])
+        else:
+            date = datetime.strptime(raw_date, '%Y-%m-%d')
+        return date
+    except:
+        exit(1)
 
 def formatDate(date):
     return date.strftime('%Y-%m-%d')
