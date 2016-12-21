bba7ad693d09711900f5dad609974f2a87081dba
rsteube <rsteube@users.noreply.github.com>
2016-12-21 22:11:14 +0100

    added argparser[WIP]

--- flik/common/dateparam.py
+++ flik/common/dateparam.py
@@ -3,33 +3,29 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta, MO, TU, WE, TH, FR, SA, SU
 
 def parse(raw_date):
-    try:
-        weekday={
-                'monday': MO(-1),        
-                'tuesday': TU(-1),        
-                'wednesday': WE(-1),        
-                'thursday': TH(-1),        
-                'friday': FR(-1),        
-                'saturday': SA(-1),        
-                'sunday': SU(-1),        
+    weekday={
+            'monday': MO(-1),
+            'tuesday': TU(-1),
+            'wednesday': WE(-1),
+            'thursday': TH(-1),
+            'friday': FR(-1),
+            'saturday': SA(-1),
+            'sunday': SU(-1),
             }
-        if raw_date == 'today':
-            date = datetime.now().date()
-        elif raw_date == 'yesterday':
-            date = datetime.now().date() - relativedelta(days=1)
-        elif raw_date in weekday:
-            date = datetime.now().date() + relativedelta(weekday=weekday[raw_date])
-        elif re.match('\d{4}-w\d{2}', raw_date) is not None:
-            fromDate = datetime.strptime(raw_date + '-1', "%Y-W%W-%w")
-            toDate = fromDate + relativedelta(days=7)
-            return fromDate, toDate
-        else:
-            date = datetime.strptime(raw_date, '%Y-%m-%d')
-        
-        toDate=date + relativedelta(days=1)
-        return date, toDate
-    except:
-        exit(1)
+    if raw_date == 'today':
+        date = datetime.now().date()
+    elif raw_date == 'yesterday':
+        date = datetime.now().date() - relativedelta(days=1)
+    elif raw_date in weekday:
+        date = datetime.now().date() + relativedelta(weekday=weekday[raw_date])
+    elif re.match('\d{4}-w\d{2}', raw_date) is not None:
+        fromDate = datetime.strptime(raw_date + '-1', "%Y-W%W-%w").date()
+        toDate = fromDate + relativedelta(days=7)
+        return fromDate, toDate
+    else:
+        date = datetime.strptime(raw_date, '%Y-%m-%d')
+    toDate=date + relativedelta(days=1)
+    return date, toDate
 
 def format(date):
     return date.strftime('%Y-%m-%d')
--- flik/flik.py
+++ flik/flik.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+import argparse
 from .common import dateparam
 
 from subprocess import call
@@ -240,11 +241,61 @@ def add_entry():
 def completion():
     print os.path.dirname(os.path.realpath(__file__)) + '/completion/zsh'
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    # activities
+    parser_activities = subparsers.add_parser('activities', help='activities help', description='List cached activities.')
+    
+    # add
+    parser_add = subparsers.add_parser('add', help='Add new Worktime entry.', description='Add new Worktime entry.')
+    parser_add.add_argument('date', metavar='date', type=str, help='the date')
+    parser_add.add_argument('project', metavar='project', type=str, help='the project')
+    parser_add.add_argument('task', metavar='task', type=str, help='the task')                 
+    parser_add.add_argument('activity', metavar='activity', type=str, help='the activity')     
+    parser_add.add_argument('billable', metavar='billable', type=str, help='desc billable', choices=['billable', 'non_billable'])
+    parser_add.add_argument('duration', metavar='duration', type=float, help='desc duration')  
+    parser_add.add_argument('comment', metavar='comment', type=str, nargs='+', help='desc duration')
+
+    # api
+    parser_api = subparsers.add_parser('api', help='api help', description='Print api.')
+    parser_api.add_argument('service', metavar='service', type=str, help='service to print')
+    
+    # del
+    parser_del = subparsers.add_parser('del', help='del help', description='Delete Worktime entry.')
+    parser_del.add_argument('date', metavar='date', type=dateparam.parse, help='the date')
+    parser_del.add_argument('workTimeID', metavar='workTimeID', type=str, help='id to delete')
+
+    parser_comp_list = subparsers.add_parser('comp_list', help='comp_list help', description='TODO')
+    parser_comp_list.add_argument('date', metavar='date', type=dateparam.parse, help='the date')
+    
+    parser_list = subparsers.add_parser('list', help='list help', description='TODO')
+    parser_list.add_argument('date', metavar='date', type=dateparam.parse, help='the date')
+
+    parser_login = subparsers.add_parser('login', help='login help', description='TODO')
+
+    parser_logout = subparsers.add_parser('logout', help='logout help', description='TODO')
+
+    parser_projects = subparsers.add_parser('projects', help='projects help', description='TODO')
+
+    parser_sync = subparsers.add_parser('sync', help='sync help', description='TODO')
+    
+    parser_tasks = subparsers.add_parser('tasks', help='tasks help', description='TODO')
+    parser_tasks.add_argument('project', metavar='project', type=str, help='the project', choices=projects(dump=False))
+
+    parser_completion = subparsers.add_parser('completion', help='completion help', description='TODO')
+   
+    print parser.parse_args()                                                         
+
 def main():
     try:
         global config
         config = {}
         config=loadConfig()
+
+        parse_args()
 
         if len(sys.argv) < 2:
 	    print 'fail'
@@ -271,5 +322,3 @@ def main():
             # suds unicode bug
             print 'SESSION_INVALID'
 
-if __name__ == "__main__":
-    main()
